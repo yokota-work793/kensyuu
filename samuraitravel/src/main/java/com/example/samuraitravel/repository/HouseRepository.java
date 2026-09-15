53073bc3768548f39ae61890a9d21265f56bf8ed
takuya <takuya@localhost>
2026-09-15 18:27:17 +0900

    260915 18:00 コミット

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -1,10 +1,14 @@
 package com.example.samuraitravel.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.samuraitravel.entity.House;
 
 // JpaRepository<エンティティのクラス型,主キーのデータ型>
 public interface HouseRepository extends JpaRepository<House, Integer> {
+
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
 	
 }
---
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.samuraitravel.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+			public Page<Role> findByNameLike(String keyword, Pageable pageable);
+			
+}
